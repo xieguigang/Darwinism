a00165c1ee36cbb4023b4b019bf9caec03c5f9a2
謝桂綱 <xie.guigang@gmail.com>
2016-02-05 18:40:40 +0800

    parser not works

--- LINQ/LINQ/LDM/Parser/Tokens/SelectClosure.vb
+++ LINQ/LINQ/LDM/Parser/Tokens/SelectClosure.vb
@@ -21,21 +21,12 @@ Namespace Statements.Tokens
         ''' <param name="parent"></param>
         Sub New(tokens As ClosureTokens(), parent As LINQStatement)
             Call MyBase.New(TokenIcer.TokenParser.Tokens.Select, tokens, parent)
-            Call Me.TryParse()
+
         End Sub
 
-        Private Sub TryParse()
-            'Dim str = Regex.Match(_statement._Original, " select .+", RegexOptions.IgnoreCase).Value
-            'For Each key In Options.OptionList
-            '    str = Regex.Split(str, String.Format(" {0}\s?", key), RegexOptions.IgnoreCase).First
-            'Next
-            'str = Mid(str, 9)
-            'MyBase._original = str
-            'If String.IsNullOrEmpty(str) Then
-            '    Throw New SyntaxErrorException("Not SELECT statement token, can not procedure the query operation!")
-            'End If
-            'Me.Expression = New LINQ.Parser.Parser().ParseExpression(str)
-        End Sub
+        Private Shared Function __isDelimiter(x As TokenIcer.Token) As Boolean
+            Return x.TokenName = TokenIcer.TokenParser.Tokens.paramDeli
+        End Function
 
         Public Sub Initialzie()
             SelectMethod = DynamicInvoke.GetMethod(MyBase._statement.ILINQProgram, SelectConstructCompiler.SelectMethodName)
