454047b09ae342df01458c5a953be7d1df94e99a
謝桂綱 <xie.guigang@gmail.com>
2016-02-05 14:41:50 +0800

    reconstruct of the linq script parser

--- LINQ/LINQ/APP/Program.vb
+++ LINQ/LINQ/APP/Program.vb
@@ -5,6 +5,29 @@
     ''' </summary>
     ''' <remarks></remarks>
     Public Function Main() As Integer
+
+        Dim s As String = "From var As Type In $source->parallel Let x = var -> aa(d,""g ++ "") where x -> test2(test3(xx),var) is true select new varType(var,x), x+3"
+        Dim t As New TokenIcer.TokenParser
+        t.InputString = s
+        Dim tk As TokenIcer.Token = Nothing
+        Dim ide As Integer = 0
+
+        Do While Not t.GetToken.ShadowCopy(tk) Is Nothing
+            Call $"[{tk.TokenName}]{tk.TokenValue}".__DEBUG_ECHO(ide)
+            If tk.TokenName = TokenIcer.TokenParser.Tokens.LPair Then
+                ide += 1
+            ElseIf tk.TokenName = TokenIcer.TokenParser.Tokens.RPair Then
+                ide -= 1
+            ElseIf tk.TokenName = TokenIcer.TokenParser.Tokens.From OrElse
+                tk.TokenName = TokenIcer.TokenParser.Tokens.In OrElse
+                tk.TokenName = TokenIcer.TokenParser.Tokens.Select OrElse
+                tk.TokenName = TokenIcer.TokenParser.Tokens.Let OrElse
+                tk.TokenName = TokenIcer.TokenParser.Tokens.Where Then
+                ide += 2
+            End If
+        Loop
+
+
         Return GetType(CLI).RunCLI(App.CommandLine, AddressOf __exeEmpty)
     End Function
 
