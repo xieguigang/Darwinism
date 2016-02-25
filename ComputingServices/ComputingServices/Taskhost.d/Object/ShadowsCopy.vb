9e1b22ecf3e792ab1809e3cdabefe28fe60a2ecd
謝桂綱 <xie.guigang@gmail.com>
2016-02-25 12:56:24 +0800

    add comments

--- ComputingServices/ComputingServices/Taskhost.d/Object/ShadowsCopy.vb
+++ ComputingServices/ComputingServices/Taskhost.d/Object/ShadowsCopy.vb
@@ -6,7 +6,7 @@ Namespace TaskHost
     Module ShadowsCopy
 
         ''' <summary>
-        ''' 
+        ''' 将客户端上面的对象数据复制到远程主机上面的内存管理模块之中
         ''' </summary>
         ''' <param name="from">Client上面的</param>
         ''' <param name="target">服务器上面的</param>
@@ -20,9 +20,9 @@ Namespace TaskHost
         ''' 
         ''' </summary>
         ''' <param name="from"></param>
-        ''' <param name="target"></param>
-        ''' <param name="memory"></param>
-        ''' <param name="stack">Avoided of the loop reference</param>
+        ''' <param name="target">远程服务器上面的对象</param>
+        ''' <param name="memory">远程对象的内存管理模块</param>
+        ''' <param name="stack">Avoided of the loop reference.(内存管理的复制堆栈记录)</param>
         ''' <returns></returns>
         Private Function __innerCopy(from As Object, target As Object, memory As MemoryHash, stack As List(Of Long)) As Boolean
             Dim props As PropertyInfo() = from.GetType.GetReadWriteProperties
@@ -33,14 +33,13 @@ Namespace TaskHost
                 If BasicTypesFlushs.ContainsKey(prop.PropertyType) Then
                     Call prop.SetValue(target, value)  ' 值类型，直接复制
                 Else   ' 引用类型，递归按址复制
-                    Dim addr = ObjectAddress.AddressOf(value)
+                    Dim addr As ObjectAddress = ObjectAddress.AddressOf(value) ' 得到对象在内存之中的位置指针
                     If memory.IsNull(addr.ReferenceAddress) Then
-                        ' 空的，则直接插入
-                        Call memory.SetObject(value)
+                        Call memory.SetObject(value)  ' 空的，则直接插入
                     Else
                         Dim innerTarget As Object = prop.GetValue(target)
 
-                        addr = ObjectAddress.AddressOf(innerTarget)
+                        addr = ObjectAddress.AddressOf(innerTarget)  ' 假若是引用类型的对象，在复制的时候还需要检查栈空间，否则会出现死循环 栈空间溢出
 
                         ' 检查栈空间是否已经复制过当前的对象了？
                         If stack.IndexOf(addr.ReferenceAddress) = -1 Then     ' 假若出现循环引用的话，应该怎样进行复制？？
