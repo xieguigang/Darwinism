43d80f2f4c3dd776472f3255245eb5ec5e146c7b
謝桂綱 <xie.guigang@gmail.com>
2016-02-29 17:02:18 +0800

    imports namespace shared

--- LINQ/LINQ/Framewok/Provider/ImportsAPI/APIProvider.vb
+++ LINQ/LINQ/Framewok/Provider/ImportsAPI/APIProvider.vb
@@ -1,5 +1,8 @@
 ﻿Namespace Framework.Provider.ImportsAPI
 
+    ''' <summary>
+    ''' 只需要记住命名空间和对应的Assembly的引用就行了
+    ''' </summary>
     Public Class APIProvider
 
     End Class
---
+++ LINQ/LINQ/Framewok/Provider/ImportsAPI/ImportsNs.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+Namespace Framework.Provider.ImportsAPI
+
+    ''' <summary>
+    ''' 导入的命名空间
+    ''' </summary>
+    Public Class ImportsNs : Inherits PackageNamespace
+
+    End Class
+End Namespace
