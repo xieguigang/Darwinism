459bc32953d4bc7a63d0a421473465be5515d676
xieguigang <xie.guigang@gmail.com>
2016-12-08 13:40:10 +0800

    namespace

--- Bootstrapping/GeneticAlgorithm/My Project/Resources.Designer.vb
+++ Bootstrapping/GeneticAlgorithm/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("GeneticAlgorithm.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("sciBasic.ComputingServices.Bootstrapping.GeneticAlgorithm.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
--- Bootstrapping/GeneticAlgorithm/My Project/Settings.Designer.vb
+++ Bootstrapping/GeneticAlgorithm/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.GeneticAlgorithm.My.MySettings
+        Friend ReadOnly Property Settings() As Global.sciBasic.ComputingServices.Bootstrapping.GeneticAlgorithm.My.MySettings
             Get
-                Return Global.GeneticAlgorithm.My.MySettings.Default
+                Return Global.sciBasic.ComputingServices.Bootstrapping.GeneticAlgorithm.My.MySettings.Default
             End Get
         End Property
     End Module
