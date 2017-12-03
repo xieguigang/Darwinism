9c27a8de2c502c307499d563d675187690c09271
xieguigang <xie.guigang@gcmodeller.org>
2017-12-03 15:06:52 +0800

    fix namespace reference error

--- ComputingServices/Asymmetric/Instance.vb
+++ ComputingServices/Asymmetric/Instance.vb
@@ -1,32 +1,33 @@
 ﻿#Region "Microsoft.VisualBasic::3918f28eab261136ec29c3a6fead9b03, ..\sciBASIC.ComputingServices\ComputingServices\Asymmetric\Instance.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xieguigang (xie.guigang@live.com)
-    '       xie (genetics@smrucc.org)
-    ' 
-    ' Copyright (c) 2016 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+'       xie (genetics@smrucc.org)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #End Region
 
 Imports System.Reflection
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.Net.SSL
 Imports Microsoft.VisualBasic.Net.TCPExtensions
 Imports sciBASIC.ComputingServices.ComponentModel
@@ -40,10 +41,10 @@ Namespace Asymmetric
 
         Protected ReadOnly _invokeCA As Net.SSL.Certificate
 
-        Sub New(CLI As CommandLine.CommandLine)
+        Sub New(CLI As CommandLine)
             Call CLI.__DEBUG_ECHO
 
-            _invokeCA = CLI(Protocols.OAuth).GetCA
+            _invokeCA = (CLI <= Protocols.OAuth).GetCA
             __host = New Net.SSL.SSLSynchronizationServicesSocket(
                 Net.TCPExtensions.GetFirstAvailablePort,
                 _invokeCA,
@@ -59,7 +60,7 @@ Namespace Asymmetric
             Call __host.Run()
         End Sub
 
-        Private Sub __returnPortal(cli As CommandLine.CommandLine)
+        Private Sub __returnPortal(cli As CommandLine)
             Call __host.WaitForStart()
             Call Microsoft.VisualBasic.Parallel.ReturnPortal(cli, __host.LocalPort)
         End Sub
