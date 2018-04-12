6bf6bfabe979dd944b9b4055360df930665bdd58
xieguigang <xie.guigang@gcmodeller.org>
2018-04-12 17:45:28 +0800

    not sure how to build fs tree

--- ossutil/FileSystem.vb
+++ ossutil/FileSystem.vb
@@ -32,8 +32,40 @@ Public Class FileSystem
 
         If Me.Bucket Is Nothing Then
             Throw New InvalidExpressionException($"Bucket name `{bucket}` is invalid or invalid ossfs credential info!")
+        Else
+            Objects = driver.ListObjects(Me.Bucket.URI(directory)).ToArray
+            CurrentDirectory = Objects.First
+            tree = FilesTree(Objects)
         End If
     End Sub
+
+    ''' <summary>
+    ''' 测试用
+    ''' </summary>
+    ''' <param name="bucket"></param>
+    ''' <param name="objects"></param>
+    ''' <param name="driver"></param>
+    Sub New(bucket As Bucket, objects As [Object](), driver As CLI)
+        Me.driver = driver
+        Me.Bucket = bucket
+        Me.Objects = objects
+        Me.CurrentDirectory = objects(0)
+
+        tree = FilesTree(objects)
+    End Sub
+
+    Private Shared Function FilesTree(objects As [Object]()) As Tree(Of Dictionary(Of String, [Object]))
+        Dim tokenTuples = objects.Select(Function(obj)
+                                             Dim path$() = obj.ObjectName _
+                                                              .Split("/"c) _
+                                                              .Skip(3) _
+                                                              .ToArray
+                                             Return (path:=path, [Object]:=obj)
+                                         End Function) _
+                                 .OrderBy(Function(obj) obj.path.Length) _
+                                 .ToArray
+
+    End Function
 
     Public Overrides Function ToString() As String
         Return $"{CurrentDirectory} @ {Bucket.BucketName}"
