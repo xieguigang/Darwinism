8a637f44916d9f8e29ca9e38444efc724beea79b
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-26 23:26:44 +0800

    fix reference errors

--- viz/ts/svg/svg/svg/test.ts
+++ viz/ts/svg/svg/svg/test.ts
@@ -4,8 +4,8 @@ $(function () {
 
     var svg = new Graphics("test-svg")
         .size(1000, 1000)
-        .drawRectangle(new Rectangle(10, 20, 200, 300))
-        .drawLine(new Pen(Color.Red(), 5), new Point(100, 200), new Point(500, 600));
+        .drawRectangle(new Canvas.Rectangle(10, 20, 200, 300))
+        .drawLine(new Canvas.Pen(Canvas.Color.Red(), 5), new Canvas.Point(100, 200), new Canvas.Point(500, 600));
 });
 
 
