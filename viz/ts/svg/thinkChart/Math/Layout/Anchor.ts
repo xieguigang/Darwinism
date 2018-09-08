bfe9802b8194efca08b66281a70937bad7891225
xieguigang <xie.guigang@gcmodeller.org>
2018-09-09 01:51:56 +0800

    add constructor

--- viz/ts/svg/thinkChart/Math/Layout/Anchor.ts
+++ viz/ts/svg/thinkChart/Math/Layout/Anchor.ts
@@ -6,5 +6,11 @@
          * Radius
         */
         public r: number;
+
+        public constructor(x: number, y: number, radius: number) {
+            super(x, y);
+
+            this.r = radius;
+        }
     }
 }
--- viz/ts/svg/thinkChart/Math/Layout/Label.ts
+++ viz/ts/svg/thinkChart/Math/Layout/Label.ts
@@ -6,6 +6,20 @@ namespace Layout {
 
         public text: string;
 
+        public get x(): number {
+            return this.left;
+        }
+
+        public get y(): number {
+            return this.top;
+        }
+
+        public constructor(geo2D: Canvas.Rectangle, label: string) {
+            super(geo2D.left, geo2D.top, geo2D.width, geo2D.height);
+
+            this.text = label;
+        }
+
         public toString() {
             return this.text;
         }
