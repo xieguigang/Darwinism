2515e00839e6bf319597335f8bad4d6c614cff5e
xieguigang <xie.guigang@gcmodeller.org>
2018-09-11 15:48:58 +0800

    config projects

--- viz/ts/svg/svg/SVG/Utils.ts
+++ viz/ts/svg/svg/SVG/Utils.ts
@@ -1,21 +1,4 @@
-/**
- * jQuery equivalent on document ready handler.
- * 
- * @param fn function for execute when the html document is load completed.
-*/
-var $ = (fn: any) => {
 
-    // Sanity check
-    if (typeof fn !== 'function') return;
-
-    // If document is already loaded, run method
-    if (document.readyState === 'complete') {
-        return fn();
-    } else {
-        // Otherwise, wait until document is loaded
-        document.addEventListener('DOMContentLoaded', fn, false);
-    }
-};
 
 module Utils {
 
