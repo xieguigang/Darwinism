a6cdf58275f35fc11a158167d12ce9ef0e43fa70
xieguigang <xie.guigang@gcmodeller.org>
2019-01-23 10:46:56 +0800

    code style improvements for run ps script

--- Docker/PowerShell.vb
+++ Docker/PowerShell.vb
@@ -2,50 +2,54 @@
 Imports System.Management.Automation
 Imports System.Management.Automation.Runspaces
 Imports System.Text
-Imports System.IO
 
+''' <summary>
+''' PowerShell interface to VB.NET
+''' </summary>
 Public Class PowerShell
 
-    ' 
-    ' 
     ''' <summary>
     ''' Takes script text as input and runs it, then converts 
     ''' the results to a string to return to the user 
     ''' </summary>
     ''' <param name="scriptText"></param>
     ''' <returns></returns>
+    ''' <remarks>
+    ''' https://blogs.msdn.microsoft.com/zainnab/2008/07/26/calling-a-powershell-script-from-your-net-code/
+    ''' </remarks>
     Public Function RunScript(scriptText As String) As String
-        ' create Powershell runspace 
-        Dim MyRunSpace As Runspace = RunspaceFactory.CreateRunspace()
-
-        ' open it 
-        MyRunSpace.Open()
-
-        ' create a pipeline and feed it the script text 
-        Dim MyPipeline As Pipeline = MyRunSpace.CreatePipeline()
-
-        MyPipeline.Commands.AddScript(scriptText)
-
-        ' add an extra command to transform the script output objects into nicely formatted strings 
-        ' remove this line to get the actual objects that the script returns. For example, the script 
-        ' "Get-Process" returns a collection of System.Diagnostics.Process instances. 
-        MyPipeline.Commands.Add("Out-String")
-
-        ' execute the script 
-        Dim results As Collection(Of PSObject) = MyPipeline.Invoke()
-
-        ' close the runspace 
-        MyRunSpace.Close()
-
         ' convert the script result into a single string 
-        Dim MyStringBuilder As New StringBuilder()
+        Dim out As New StringBuilder()
+        Dim results As Collection(Of PSObject) = RunPSScript(scriptText)
 
         For Each obj As PSObject In results
-            MyStringBuilder.AppendLine(obj.ToString())
+            out.AppendLine(obj.ToString())
         Next
 
         ' return the results of the script that has 
         ' now been converted to text 
-        Return MyStringBuilder.ToString()
+        Return out.ToString()
+    End Function
+
+    Public Function RunPSScript(ps As String) As Collection(Of PSObject)
+        ' create Powershell runspace 
+        Using myRunSpace As Runspace = RunspaceFactory.CreateRunspace()
+            ' open it 
+            Call myRunSpace.Open()
+
+            ' create a pipeline and feed it the script text 
+            With myRunSpace.CreatePipeline()
+                Call .Commands.AddScript(ps)
+                ' add an extra command to transform the script output objects 
+                ' into nicely formatted strings remove this line to get the 
+                ' actual objects that the script returns. For example, the script 
+                ' "Get-Process" returns a collection of System.Diagnostics.Process 
+                ' instances.
+                Call .Commands.Add("Out-String")
+
+                ' execute the script 
+                Return .Invoke()
+            End With
+        End Using
     End Function
 End Class
