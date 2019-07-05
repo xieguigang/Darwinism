140309da4ac77dcec83f23f32c8cd0b53458dbf7
xieguigang <xie.guigang@gcmodeller.org>
2019-07-06 00:27:20 +0800

    update webbrowser data throught websocket

---
+++ viz/ANNWebSocket/Package.vb
@@ -0,0 +1,11 @@
+﻿Public Class Package
+
+    ''' <summary>
+    ''' The error of fitting in current training iteration
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [error] As Double
+    Public Property iteration As Integer
+
+
+End Class
