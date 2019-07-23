0a06dc853db2b26bb52f7a01b3a315a10cd201be
xieguigang <xie.guigang@gcmodeller.org>
2019-07-23 16:16:46 +0800

    add protocol entries

---
+++ ComputingServices/DistributeServices/Registry/RegistryProtocol.vb
@@ -0,0 +1,17 @@
+﻿Namespace DistributeServices
+
+    Public Enum RegistryProtocols
+        ''' <summary>
+        ''' Send a ping to ask if it is a grid node?
+        ''' </summary>
+        IsANode
+        ''' <summary>
+        ''' Send a ping to ask if it is a registry of the <see cref="CenterController"/>
+        ''' </summary>
+        IsAPool
+        ''' <summary>
+        ''' A grid node sign up and registry in this computing grid
+        ''' </summary>
+        SignUp
+    End Enum
+End Namespace
