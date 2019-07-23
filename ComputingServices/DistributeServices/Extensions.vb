c5b309e903c7b0c87879f5afaba7ad1be64a1b56
xieguigang <xie.guigang@gcmodeller.org>
2019-07-23 15:47:08 +0800

    how to scan network?

---
+++ ComputingServices/DistributeServices/Extensions.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace DistributeServices
+
+    <HideModuleName>
+    Public Module Extensions
+
+        <Extension>
+        Friend Iterator Function GetIPAddressList(IPrange As String) As IEnumerable(Of String)
+
+        End Function
+    End Module
+End Namespace
--- ComputingServices/DistributeServices/Registry.vb
+++ ComputingServices/DistributeServices/Registry.vb
@@ -11,21 +11,35 @@ Namespace DistributeServices
         ''' 这是一个网络范围,不是一个具体的IP端点
         ''' </summary>
         ReadOnly grid As IPEndPoint
+        ReadOnly nodes As New List(Of IPEndPoint)
 
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="ipRange">
+        ''' IP地址范围表达式:
+        ''' 
+        ''' 1. Use all of the ip address in range: ``192.168.*``
+        ''' 2. Use selected ip address in range: ``192.168.1.* - 192.168.10.200``
+        ''' 
+        ''' </param>
+        ''' <param name="port%"></param>
         Sub New(ipRange$, port%)
             grid = New IPEndPoint(ipRange, port)
         End Sub
 
         Public Sub doScanForNodes()
+            For Each ip As String In DistributeServices.GetIPAddressList(grid.IPAddress)
 
+            Next
         End Sub
 
         Public Function GetEnumerator() As IEnumerator(Of IPEndPoint) Implements IEnumerable(Of IPEndPoint).GetEnumerator
-            Throw New NotImplementedException()
+            Return nodes.GetEnumerator
         End Function
 
-        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-            Throw New NotImplementedException()
+        Private Iterator Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Yield GetEnumerator()
         End Function
     End Class
 End Namespace
