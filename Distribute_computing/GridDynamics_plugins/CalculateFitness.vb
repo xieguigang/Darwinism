ec0cfd45680fd98ad9198afc6df863a51b926280
xieguigang <xie.guigang@gcmodeller.org>
2019-07-24 10:19:26 +0800

    config projects

--- Distribute_computing/GridDynamics_plugins/CalculateFitness.vb
+++ Distribute_computing/GridDynamics_plugins/CalculateFitness.vb
@@ -1,9 +1,18 @@
 ﻿Imports Microsoft.VisualBasic.ApplicationServices.Plugin
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.Models
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.NonlinearGridTopology
 
 Public Module CalculateFitness
 
     <Plugin("GA/multiple_process")>
-    Public Function MultipleProcessParallel()
+    Public Function MultipleProcessParallel(comparator As FitnessPool(Of Genome), source As IEnumerable(Of Genome)) As IEnumerable(Of NamedValue(Of Double))
+        Dim individuals As Genome() = source.ToArray
+        Dim partitionSize = individuals.Length / App.CPUCoreNumbers
+        Dim partitions = individuals.Split(partitionSize)
+
+        ' 在这里folk出多条进程进行并行计算
+        ' 这个方法可以极大的提升程序在Linux平台上面的计算效率
 
     End Function
 
