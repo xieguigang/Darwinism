bc9dacf94324017cd7a5e573ce96776d16571792
xieguigang <xie.guigang@gcmodeller.org>
2019-07-24 10:10:16 +0800

    add parallel plugin for GA

---
+++ GridDynamics_plugins/CalculateFitness.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Plugin
+
+Public Module CalculateFitness
+
+    <Plugin("GA/multiple_process")>
+    Public Function MultipleProcessParallel()
+
+    End Function
+
+    Public Function DistributionComputing()
+
+    End Function
+End Module
