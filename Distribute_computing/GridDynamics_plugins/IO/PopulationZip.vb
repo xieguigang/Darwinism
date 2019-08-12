3884896ab1c1821a3f96d4ac24121cac2ca59946
xieguigang <xie.guigang@gcmodeller.org>
2019-08-12 14:15:07 +0800

    write grid system into zip

--- Distribute_computing/GridDynamics_plugins/IO/PopulationZip.vb
+++ Distribute_computing/GridDynamics_plugins/IO/PopulationZip.vb
@@ -1,4 +1,7 @@
-﻿Imports Microsoft.VisualBasic.MachineLearning.Darwinism.NonlinearGridTopology
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports Microsoft.VisualBasic.ApplicationServices.Zip
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.NonlinearGridTopology
 
 ''' <summary>
 ''' 使用zip压缩的形式，将population保存为临时文件
@@ -16,6 +19,20 @@ Public Class PopulationZip
     End Sub
 
     Public Sub Add(genome As GridSystem)
+        Dim temp = App.GetAppSysTempFile(".grid", App.PID, "population_")
 
+        Using file As FileStream = temp.Open
+            Call genome.Serialize(file, chunkSize:=2048)
+        End Using
+
+        Call ZipLib.AddToArchive(
+            files:={temp},
+            archiveFullName:=target,
+            action:=ArchiveAction.Merge,
+            fileOverwrite:=Overwrite.Always,
+            compression:=CompressionLevel.Fastest
+        )
+
+        Call temp.DeleteFile
     End Sub
 End Class
