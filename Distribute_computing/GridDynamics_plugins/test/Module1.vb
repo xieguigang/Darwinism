85fc6cf739fb7f3da962aea1604b95b47282f4b3
xieguigang <xie.guigang@gcmodeller.org>
2019-08-12 10:43:52 +0800

    gzip bugs?

---
+++ Distribute_computing/GridDynamics_plugins/test/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports GridDynamics_plugins
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+Module Module1
+
+    Sub Main()
+
+        Dim vec As Vector = 50000.Sequence.Select(Function(i) CDbl(i)).AsVector
+        Dim filePath$ = "./test.dat"
+
+
+        Using file = filePath.Open
+            Call vec.Serialize(file)
+        End Using
+    End Sub
+
+End Module
