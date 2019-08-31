d78e8a1dab1b3f2a79ea43fc83af5d7b76f52185
xieguigang <xie.guigang@gcmodeller.org>
2019-08-31 13:05:29 +0800

    socket model

---
+++ Distribute_computing/ManagerInterface/SocketHost/InterfaceSocket.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Net.Sockets
+Imports SMRUCC.WebCloud.HTTPInternal.Core.WebSocket
+
+Public Class InterfaceSocket : Inherits WsProcessor
+
+    Public Sub New(tcp As TcpClient)
+        MyBase.New(tcp)
+    End Sub
+End Class
