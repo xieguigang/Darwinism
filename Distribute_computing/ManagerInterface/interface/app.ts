61f209323ebeec1a3f8da1d0f2e3c023bcbc3ec8
xieguigang <xie.guigang@gcmodeller.org>
2019-09-01 01:40:55 +0800

    draw interface with d3.js?

---
+++ Distribute_computing/ManagerInterface/interface/app.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path="../linq.d.ts" />
+
+interface GridNode {
+
+    guid: string;
+    status: nodeStatus;
+    ip: string;
+    cpu_load: number;
+    memory_load: number;
+    memory_installed: number;
+    memory_used: number;
+
+}
+
+enum nodeStatus {
+    busy, offline, idle
+}
+
