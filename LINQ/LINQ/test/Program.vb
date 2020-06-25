2d094e1eb3dbd1ad9d791b6fa4da46d18ed1a60d
xieguigang <xie.guigang@gcmodeller.org>
2020-06-25 20:49:32 +0800

    run script parser test

--- LINQ/LINQ/test/Program.vb
+++ LINQ/LINQ/test/Program.vb
@@ -1,3 +1,13 @@
-﻿Public Class Program
+﻿Public Module Program
 
-End Class
+    Sub Main()
+        Call parserTest()
+    End Sub
+
+    Sub parserTest()
+        Dim script = "from x as double in `http://aaa.txt` where x > 5 select x ^ 2 # this is comment text"
+        Dim tokens = LINQ.Language.GetTokens(script).ToArray
+
+        Pause()
+    End Sub
+End Module
