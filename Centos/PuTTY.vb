e17c51dd1f1ed57be478bfa62dacc0e368a0525d
xieguigang <xie.guigang@gcmodeller.org>
2020-12-02 16:22:27 +0800

    unsure for the plink command line

--- Centos/PuTTY.vb
+++ Centos/PuTTY.vb
@@ -41,7 +41,7 @@ Public Class PuTTY
 
     Public Function Shell(command As String, Optional arguments As String = Nothing) As String
         Dim cmdl As String = If(arguments.StringEmpty, command, $"{command} {arguments}")
-        Dim cli As String = $"{endpoint} -P {port} -l {user} -pw ""{password}"" bash -c ""{cmdl}"""
+        Dim cli As String = $"{user}@{endpoint} -P {port} -pw ""{password}"" bash -c ""{cmdl}"""
         Dim std_out As String = CommandLine.Call(plink, cli)
 
         Return std_out
