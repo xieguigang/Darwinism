f0e382c8173447629a030cd9ab8664bf1e7441bd
xieguigang <xie.guigang@gcmodeller.org>
2020-12-02 16:00:09 +0800

    run debug

--- Docker/test/Module1.vb
+++ Docker/test/Module1.vb
@@ -1,51 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::d3d37a19a2dec6b62f31fa6da5d95335, Docker\test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Module1
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module Module1
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Darwinism
+Imports Darwinism.Centos
 Imports Darwinism.Docker.Arguments
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Module1
 
     Sub Main()
+
+        Call testPuTTY()
+
         Dim ps As New Docker.PowerShell
 
         ' Call Console.WriteLine(ps.RunScript("docker ps"))
@@ -65,4 +69,11 @@ Module Module1
         Pause()
     End Sub
 
+    Sub testPuTTY()
+        Dim ssh As New PuTTY("xcms", "12345", "192.168.1.253")
+
+        Call Console.WriteLine(ssh.Shell("ls", "-l /bin"))
+
+        Pause()
+    End Sub
 End Module
