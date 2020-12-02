0f6671a4bcb5571ed03d2d2ee86ff638d676a594
xieguigang <xie.guigang@gcmodeller.org>
2020-12-02 15:31:43 +0800

    putty bash

--- Centos/PuTTY.vb
+++ Centos/PuTTY.vb
@@ -7,17 +7,19 @@ Public Class PuTTY
     ReadOnly user As String
     ReadOnly password As String
     ReadOnly endpoint As String
+    ReadOnly port As Integer
     ReadOnly plink As String
 
-    Sub New(user$, password$, Optional endpoint$ = "127.0.0.1:22", Optional plink As String = "plink")
+    Sub New(user$, password$, Optional endpoint$ = "127.0.0.1", Optional port% = 22, Optional plink As String = "plink")
         Me.user = user
         Me.password = password
         Me.endpoint = endpoint
         Me.plink = plink
+        Me.port = port
     End Sub
 
     Public Overrides Function ToString() As String
-        Return $"ssh {user}@{endpoint}"
+        Return $"ssh {user}@{endpoint}:{port}"
     End Function
 
     ''' <summary>
@@ -30,8 +32,8 @@ Public Class PuTTY
     ''' 
     ''' https://stackoverflow.com/questions/57778301/hyper-v-powershell-run-a-bash-command-in-linux-vm-and-get-output
     ''' </remarks>
-    Public Function Shell(bash As String) As String
-        Dim cli As String = $"{endpoint} -l {user}L -pw {password}L -batch bash ""{bash}"""
+    Public Function Run(bash As String) As String
+        Dim cli As String = $"{endpoint} -P {port} -l {user} -pw ""{password}"" -batch bash ""{bash}"""
         Dim std_out As String = CommandLine.Call(plink, cli)
 
         Return std_out
