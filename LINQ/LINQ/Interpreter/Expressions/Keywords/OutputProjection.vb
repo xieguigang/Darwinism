812c5778b1e0280ce0a82d8985efdc24f367f9cd
xieguigang <xie.guigang@gcmodeller.org>
2021-01-16 16:10:57 +0800

    linq keyword expression

---
+++ LINQ/LINQ/Interpreter/Expressions/Keywords/OutputProjection.vb
@@ -0,0 +1,22 @@
+﻿Imports LINQ.Runtime
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Interpreter.Expressions
+
+    Public Class OutputProjection : Inherits Expression
+
+        Public Property fields As NamedValue(Of Expression)()
+
+        Sub New(fields As IEnumerable(Of NamedValue(Of Expression)))
+            Me.fields = fields.ToArray
+        End Sub
+
+        Public Overrides Function Exec(env As Environment) As Object
+            Throw New NotImplementedException()
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"new {{{fields.Select(Function(a) $"{a.Name} = {a.Value}").JoinBy(", ")}}}"
+        End Function
+    End Class
+End Namespace
