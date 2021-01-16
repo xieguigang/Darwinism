2cd0c12a1cb8ac17f5f9f3ce167cbf1d55f48a9d
xieguigang <xie.guigang@gcmodeller.org>
2021-01-16 13:53:37 +0800

    parse blocks

--- LINQ/LINQ/Script/StackParser.vb
+++ LINQ/LINQ/Script/StackParser.vb
@@ -18,18 +18,37 @@ Module StackParser
         Return isKeyword(t, "select")
     End Function
 
+    Private Function GetParentPair(t As String) As String
+        Select Case t
+            Case "]" : Return "["
+            Case "}" : Return "{"
+            Case ")" : Return "("
+            Case Else
+                Throw New InvalidExpressionException
+        End Select
+    End Function
+
+    <Extension>
+    Public Iterator Function SplitByTopLevelStack(tokenList As IEnumerable(Of Token)) As IEnumerable(Of Token())
+        For Each block In tokenList.DoSplitByTopLevelStack
+            If Not block.All(Function(t) t.name = Tokens.Terminator) Then
+                Yield block
+            End If
+        Next
+    End Function
+
     ''' <summary>
     ''' 根据最顶端的关键词以及括号进行栈片段的分割
     ''' </summary>
     ''' <param name="tokenList"></param>
     ''' <returns></returns>
     <Extension>
-    Public Iterator Function SplitByTopLevelStack(tokenList As IEnumerable(Of Token)) As IEnumerable(Of Token())
+    Private Iterator Function DoSplitByTopLevelStack(tokenList As IEnumerable(Of Token)) As IEnumerable(Of Token())
         Dim block As New List(Of Token)
         Dim stack As New Stack(Of String)
 
-        For Each item As Token In tokenList
-            If item.name = Tokens.keyword Then
+        For Each item As Token In tokenList.Where(Function(t) t.name <> Tokens.Terminator AndAlso t.name <> Tokens.Comment)
+            If item.name = Tokens.keyword AndAlso Not item.text.TextEquals("as") Then
                 If stack.Count > 0 Then
                     block.Add(item)
                 Else
@@ -54,7 +73,7 @@ Module StackParser
             ElseIf item.name = Tokens.Close Then
                 Dim parent As String = stack.Pop
 
-                If parent <> item.text Then
+                If parent <> GetParentPair(item.text) Then
                     Throw New SyntaxErrorException
                 End If
 
