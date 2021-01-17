3cc9d8f173c841a74ef1640e88c022d3950375a4
xieguigang <xie.guigang@gcmodeller.org>
2021-01-17 13:29:48 +0800

    Timestamp value

---
+++ Google.Protobuf/WellKnownTypes/Timestamp/TimestampReflection.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.Language
+Imports pbr = Google.Protobuf.Reflection
+
+Namespace Google.Protobuf.WellKnownTypes
+
+    ''' <summary>Holder for reflection information generated from google/protobuf/timestamp.proto</summary>
+    Partial Public Module TimestampReflection
+
+#Region "Descriptor"
+        ''' <summary>File descriptor for google/protobuf/timestamp.proto</summary>
+        Public ReadOnly Property Descriptor As pbr.FileDescriptor
+            Get
+                Return descriptorField
+            End Get
+        End Property
+
+        Private descriptorField As pbr.FileDescriptor
+
+        Sub New()
+            Dim descriptorData As Byte() = Global.System.Convert.FromBase64String(String.Concat("Ch9nb29nbGUvcHJvdG9idWYvdGltZXN0YW1wLnByb3RvEg9nb29nbGUucHJv", "dG9idWYiKwoJVGltZXN0YW1wEg8KB3NlY29uZHMYASABKAMSDQoFbmFub3MY", "AiABKAVCgQEKE2NvbS5nb29nbGUucHJvdG9idWZCDlRpbWVzdGFtcFByb3Rv", "UAFaK2dpdGh1Yi5jb20vZ29sYW5nL3Byb3RvYnVmL3B0eXBlcy90aW1lc3Rh", "bXCgAQH4AQGiAgNHUEKqAh5Hb29nbGUuUHJvdG9idWYuV2VsbEtub3duVHlw", "ZXNiBnByb3RvMw=="))
+            descriptorField = pbr.FileDescriptor.FromGeneratedCode(descriptorData, New pbr.FileDescriptor() {}, New pbr.GeneratedClrTypeInfo(Nothing, New pbr.GeneratedClrTypeInfo() {New pbr.GeneratedClrTypeInfo(GetType(Global.Google.Protobuf.WellKnownTypes.Timestamp), Global.Google.Protobuf.WellKnownTypes.Timestamp.Parser, {"Seconds", "Nanos"}, Nothing, Nothing, Nothing)}))
+        End Sub
+#End Region
+
+    End Module
+End Namespace
