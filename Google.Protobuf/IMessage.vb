e2623ea1b85c09e81a7135f3627cb5bb0a6bd2ad
xieguigang <xie.guigang@gcmodeller.org>
2021-01-17 13:01:51 +0800

    IMessageType

--- Google.Protobuf/IMessage.vb
+++ Google.Protobuf/IMessage.vb
@@ -67,19 +67,4 @@ Namespace Google.Protobuf
         ReadOnly Property Descriptor As MessageDescriptor
     End Interface
 
-    ''' <summary>
-    ''' Generic interface for a Protocol Buffers message,
-    ''' where the type parameter is expected to be the same type as
-    ''' the implementation class.
-    ''' </summary>
-    ''' <typeparam name="T">The message type.</typeparam>
-    Public Interface IMessageType(Of T As IMessageType(Of T))
-        Inherits IMessage, IEquatable(Of T), IDeepCloneable(Of T)
-        ''' <summary>
-        ''' Merges the given message into this one.
-        ''' </summary>
-        ''' <remarks>See the user guide for precise merge semantics.</remarks>
-        ''' <param name="message">The message to merge with this one. Must not be null.</param>
-        Overloads Sub MergeFrom(message As T)
-    End Interface
 End Namespace
