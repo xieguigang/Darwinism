cc87256ce58dddfd69ead827646447370d969b3c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-13 13:58:31 +0800

    Update SocketRef.vb

--- Parallel/IpcParallel/SocketBuffer/SocketRef.vb
+++ Parallel/IpcParallel/SocketBuffer/SocketRef.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::feb839fe206c8518a09ae7577064ddbf, Parallel\IpcParallel\SocketBuffer\SocketRef.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class SocketRef
-    ' 
-    '         Properties: address
-    ' 
-    '         Function: CreateReference, GetSocket, Open, ToString, WriteBuffer
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class SocketRef
+' 
+'         Properties: address
+' 
+'         Function: CreateReference, GetSocket, Open, ToString, WriteBuffer
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices
 Imports Microsoft.VisualBasic.MIME.application.json.BSON
 
 Namespace IpcStream
@@ -95,7 +96,7 @@ Namespace IpcStream
 
         <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Function CreateReference() As SocketRef
-            Return App.CreateTempFilePath(App.GetVariable("sockets", App.SysTemp), ".sock", App.PID.ToHexString, prefix:="Parallel")
+            Return TempFileSystem.CreateTempFilePath(App.GetVariable("sockets", App.SysTemp), ".sock", App.PID.ToHexString, prefix:="Parallel")
         End Function
 
         Public Shared Sub SetSocketPool(handle As String)
