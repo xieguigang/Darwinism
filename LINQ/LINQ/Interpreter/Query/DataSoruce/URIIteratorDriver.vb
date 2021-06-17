e94cdb34ea2544334cfbead811bd147aca8e9479
xieguigang <xie.guigang@gcmodeller.org>
2021-06-17 18:59:25 +0800

    fix of namespace reference errors

--- LINQ/LINQ/Interpreter/Query/DataSoruce/URIIteratorDriver.vb
+++ LINQ/LINQ/Interpreter/Query/DataSoruce/URIIteratorDriver.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::404fd56e2359587df6a9467d5cad52cb, LINQ\LINQ\Interpreter\Query\DataSoruce\URIIteratorDriver.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class URIIteratorDriver
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: PopulatesData
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class URIIteratorDriver
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: PopulatesData
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports LINQ.Interpreter.Expressions
 Imports LINQ.Runtime
+Imports LINQ.Runtime.Drivers
 
 Namespace Interpreter.Query
 
--- LINQ/LINQ/Runtime/Environment.vb
+++ LINQ/LINQ/Runtime/Environment.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::1f5d8b88408af1f1f9a33a6e64bc4098, LINQ\LINQ\Runtime\Environment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Environment
-    ' 
-    '         Properties: GlobalEnvir, IsGlobal
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: AddSymbol, FindInvoke, FindSymbol, HasSymbol
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Environment
+' 
+'         Properties: GlobalEnvir, IsGlobal
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: AddSymbol, FindInvoke, FindSymbol, HasSymbol
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports System.Runtime.CompilerServices
+Imports LINQ.Runtime.Internal
 
 Namespace Runtime
 
@@ -53,6 +54,7 @@ Namespace Runtime
         Protected ReadOnly parent As Environment
 
         Public ReadOnly Property IsGlobal As Boolean
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return parent Is Nothing
             End Get
@@ -68,10 +70,12 @@ Namespace Runtime
             End Get
         End Property
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Sub New(parent As Environment)
             Me.parent = parent
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function FindInvoke(name As String) As Callable
             Return InternalInvoke.FindInvoke(name)
         End Function
