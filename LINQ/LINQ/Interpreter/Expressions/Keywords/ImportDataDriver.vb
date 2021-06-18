3c51a7eb243e81b9feb3ca6ccc6074af1c2b1733
xieguigang <xie.guigang@gcmodeller.org>
2021-06-19 01:24:51 +0800

    imports data driver

---
+++ LINQ/LINQ/Interpreter/Expressions/Keywords/ImportDataDriver.vb
@@ -0,0 +1,21 @@
+﻿Namespace Interpreter.Expressions
+
+    Public Class ImportDataDriver : Inherits KeywordExpression
+
+        Public ReadOnly Property dllName As String
+
+        Public Overrides ReadOnly Property keyword As String
+            Get
+                Return "Imports"
+            End Get
+        End Property
+
+        Sub New(dllName As String)
+            Me.dllName = dllName
+        End Sub
+
+        Public Overrides Function Exec(context As ExecutableContext) As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
