68904d3ccb9cc47839c546c3331290a48c873efb
xieguigang <xie.guigang@gcmodeller.org>
2021-06-19 02:23:46 +0800

    CDF dataframe driver?

---
+++ LINQ/Drivers/NetCDF/CDFDataSet.vb
@@ -0,0 +1,13 @@
+﻿Imports LINQ.Runtime.Drivers
+
+<DriverFlag("dataframe")>
+Public Class CDFDataSet : Inherits DataSourceDriver
+
+    Public Sub New(arguments() As String)
+        MyBase.New(arguments)
+    End Sub
+
+    Public Overrides Function ReadFromUri(uri As String) As IEnumerable(Of Object)
+        Throw New NotImplementedException()
+    End Function
+End Class
