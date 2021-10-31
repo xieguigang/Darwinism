687e0f3ec1eccce88f6a12e1504df1eca35e2ae4
xieguigang <xie.guigang@gcmodeller.org>
2021-10-31 15:47:50 +0800

    Update ThreadTask.vb

--- Parallel/ThreadTask/ThreadTask.vb
+++ Parallel/ThreadTask/ThreadTask.vb
@@ -1,44 +1,44 @@
-﻿#Region "Microsoft.VisualBasic::b63a63c50d6beec5245bbe02d8f84e13, Parallel\ThreadTask\ThreadTask.vb"
+﻿#Region "Microsoft.VisualBasic::b3dee4ed59d69548e25c89b2f6a27bd2, Parallel\ThreadTask\ThreadTask.vb"
 
-' Author:
-' 
-'       asuka (amethyst.asuka@gcmodeller.org)
-'       xie (genetics@smrucc.org)
-'       xieguigang (xie.guigang@live.com)
-' 
-' Copyright (c) 2018 GPL3 Licensed
-' 
-' 
-' GNU GENERAL PUBLIC LICENSE (GPL3)
-' 
-' 
-' This program is free software: you can redistribute it and/or modify
-' it under the terms of the GNU General Public License as published by
-' the Free Software Foundation, either version 3 of the License, or
-' (at your option) any later version.
-' 
-' This program is distributed in the hope that it will be useful,
-' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-' GNU General Public License for more details.
-' 
-' You should have received a copy of the GNU General Public License
-' along with this program. If not, see <http://www.gnu.org/licenses/>.
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-' /********************************************************************************/
+    ' /********************************************************************************/
 
-' Summaries:
+    ' Summaries:
 
-'     Class ThreadTask
-' 
-'         Constructor: (+1 Overloads) Sub New
-'         Function: (+2 Overloads) CreateThreads, GetCompleteThread, GetEmptyThread, ParallelTask, RunParallel
-'                   SequenceTask, ToString, WithDegreeOfParallelism
-' 
-' 
-' /********************************************************************************/
+    '     Class ThreadTask
+    ' 
+    '         Constructor: (+1 Overloads) Sub New
+    '         Function: (+2 Overloads) CreateThreads, GetCompleteThread, GetEmptyThread, ParallelTask, RunParallel
+    '                   SequenceTask, ToString, WithDegreeOfParallelism
+    ' 
+    ' 
+    ' /********************************************************************************/
 
 #End Region
 
