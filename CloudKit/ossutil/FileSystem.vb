9e750563f2aa4711e911a251c32fdb8896bf2181
guigang xie <gg.xie@bionovogene.com>
2021-12-13 13:46:14 +0800

    fix for .net 5 project

--- CloudKit/ossutil/FileSystem.vb
+++ CloudKit/ossutil/FileSystem.vb
@@ -1,50 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::7fbdf0cd02b0bf1d9cdbe276ae8d02b6, CloudKit\ossutil\FileSystem.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Class FileSystem
-    ' 
-    '     Properties: Bucket, CurrentDirectory, Objects
-    ' 
-    '     Constructor: (+4 Overloads) Sub New
-    ' 
-    '     Function: [Get], ChangeDirectory, FilesTree, GetContext, GetTarget
-    '               ToString
-    ' 
-    '     Sub: Put
-    ' 
-    ' /********************************************************************************/
+' Class FileSystem
+' 
+'     Properties: Bucket, CurrentDirectory, Objects
+' 
+'     Constructor: (+4 Overloads) Sub New
+' 
+'     Function: [Get], ChangeDirectory, FilesTree, GetContext, GetTarget
+'               ToString
+' 
+'     Sub: Put
+' 
+' /********************************************************************************/
 
 #End Region
+
+#If netcore5 = 1 Then
+Imports System.Data
+#End If
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.GraphTheory
@@ -166,7 +170,7 @@ Public Class FileSystem
         Dim node As Tree(Of [Object])
         Dim key$
         Dim root As New Tree(Of [Object])("/") With {
-            .Label = $"oss://{bucketName}",
+            .label = $"oss://{bucketName}",
             .Childs = New Dictionary(Of String, Tree(Of [Object])),
             .Data = New [Object] With {
                 .meta = New Dictionary(Of String, String) From {
@@ -187,7 +191,7 @@ Public Class FileSystem
                     Else
                         If Not node.Childs.ContainsKey(key) Then
                             Dim newNode As New Tree(Of [Object])("/") With {
-                                .Label = key,
+                                .label = key,
                                 .Childs = New Dictionary(Of String, Tree(Of [Object])),
                                 .Parent = node
                             }
@@ -268,7 +272,7 @@ Public Class FileSystem
     ''' <summary>
     ''' 获取得到临时文件路径
     ''' </summary>
-    Shared tempFile As New [Default](Of  String)(AddressOf populateTempFile, isLazy:=False)
+    Shared tempFile As New [Default](Of String)(AddressOf populateTempFile, isLazy:=False)
 
     ''' <summary>
     ''' Get file from OSS
--- CloudKit/ossutil/My Project/AssemblyInfo.vb
+++ CloudKit/ossutil/My Project/AssemblyInfo.vb
@@ -7,7 +7,7 @@ Imports System.Runtime.InteropServices
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
-
+#if netcore5=0 then
 <Assembly: AssemblyTitle("Aliyun ossutil CLI tool wrapper for VB.NET")>
 <Assembly: AssemblyDescription("https://github.com/aliyun/ossutil")>
 <Assembly: AssemblyCompany("I@xieguigang.me")>
@@ -33,3 +33,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
+#end if
