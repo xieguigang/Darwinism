0fede2486670da3d729538939a9dcb73df9904aa
xieguigang <xie.guigang@gcmodeller.org>
2023-08-03 16:33:00 +0800

    use a custom attribute for define a custom IPC parallel process?

---
+++ src/Parallel/IpcParallel/AppHost.vb
@@ -0,0 +1,31 @@
+﻿
+''' <summary>
+''' define the application host information for the IPC parallel
+''' </summary>
+''' 
+Public Class AppHost : Inherits Attribute
+
+    ''' <summary>
+    ''' executable name of the application host
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property AppName As String
+    ''' <summary>
+    ''' the commandline argument string that could be used as the protocol handler of this IPC parallel
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Protocol As String
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="app"></param>
+    ''' <param name="args">
+    ''' the commandline arguments template string
+    ''' </param>
+    Sub New(app As String, args As String)
+        AppName = app
+        Protocol = args
+    End Sub
+
+End Class
