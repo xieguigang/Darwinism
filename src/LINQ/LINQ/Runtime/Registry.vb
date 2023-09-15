81c4243988423b11d70eb4be52d18573dc4123df
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-15 20:00:43 +0800

    make bugs fixed for port to .net 6 platform

--- src/LINQ/LINQ/Runtime/Registry.vb
+++ src/LINQ/LINQ/Runtime/Registry.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::9a4a81cd0a50303885a339f5c960d722, LINQ\LINQ\Runtime\Registry.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Registry
-    ' 
-    '         Function: GetReader, GetTypeCodeName
-    ' 
-    '         Sub: Register
-    ' 
-    '     Delegate Function
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Registry
+' 
+'         Function: GetReader, GetTypeCodeName
+' 
+'         Sub: Register
+' 
+'     Delegate Function
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Data
 Imports System.Reflection
 Imports LINQ.Runtime.Drivers
-Imports Microsoft.VisualBasic.Language
 
 Namespace Runtime
 
