a9b9ece35bfe33fe10034d9d32a5ee8828926b07
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-25 09:41:30 +0800

    export functions

---
+++ src/Darwinism/Env.vb
@@ -0,0 +1,23 @@
+﻿
+Imports Darwinism.DataScience.DataMining
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' the IPC parallel environment
+''' </summary>
+<Package("Environment")>
+Module Env
+
+    ''' <summary>
+    ''' set the parallel batch threads
+    ''' </summary>
+    ''' <param name="n_threads"></param>
+    ''' <returns></returns>
+    <ExportAPI("set_threads")>
+    Public Function Set_threads(n_threads As Integer) As Object
+        Call VectorMath.SetThreads(n_threads)
+        Return Nothing
+    End Function
+
+End Module
---
+++ src/Darwinism/Math.vb
@@ -0,0 +1,28 @@
+﻿Imports Darwinism.DataScience.DataMining
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.DataMining.KMeans
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+''' <summary>
+''' math helpers
+''' </summary>
+<Package("Math")>
+Module Math
+
+    ''' <summary>
+    ''' measure the average distance for the given dataset
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("average_distance")>
+    Public Function averageDistance(<RRawVectorArgument> x As Object, Optional env As Environment = Nothing) As Double
+        Dim bigDataset As ClusterEntity()
+        Dim dist As Double = VectorMath.AverageDistance(bigDataset)
+
+        Return dist
+    End Function
+
+End Module
