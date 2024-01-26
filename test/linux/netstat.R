7d0cd45839594bdffca0d30a351f689b09135729
谢桂纲 <gg.xie@bionovogene.com>
2024-01-26 09:23:35 +0800

    configs of run demo test

---
+++ test/linux/netstat.R
@@ -0,0 +1,26 @@
+require(Darwinism);
+
+const text1 = "
+
+(Not all processes could be identified, non-owned process info
+ will not be shown, you would have to be root to see it all.)
+Active Internet connections (only servers)
+Proto Recv-Q Send-Q Local Address           Foreign Address         State       PID/Program name
+tcp        0      0 0.0.0.0:22              0.0.0.0:*               LISTEN      -
+tcp        0      0 0.0.0.0:111             0.0.0.0:*               LISTEN      -
+tcp        0      0 127.0.0.1:631           0.0.0.0:*               LISTEN      -
+tcp6       0      0 :::22                   :::*                    LISTEN      -
+tcp6       0      0 :::111                  :::*                    LISTEN      -
+tcp6       0      0 ::1:631                 :::*                    LISTEN      -
+udp        0      0 0.0.0.0:111             0.0.0.0:*                           -
+udp        0      0 127.0.0.1:323           0.0.0.0:*                           -
+udp        0      0 0.0.0.0:5353            0.0.0.0:*                           -
+udp        0      0 0.0.0.0:34247           0.0.0.0:*                           -
+udp6       0      0 :::43332                :::*                                -
+udp6       0      0 :::111                  :::*                                -
+udp6       0      0 ::1:323                 :::*                                -
+udp6       0      0 :::5353                 :::*                                -
+
+";
+
+print(centos::netstat(text1));
