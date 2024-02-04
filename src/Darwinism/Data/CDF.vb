2a662a41a1a43cc0bd24155ba0795574a9fd05d6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-04 17:15:08 +0800

    how to get data

--- src/Darwinism/Data/CDF.vb
+++ src/Darwinism/Data/CDF.vb
@@ -1,6 +1,5 @@
 ﻿Imports CDF.PInvoke
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.DataStorage.netCDF.Data
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("CDF")>
@@ -18,19 +17,8 @@ Module CDF
     ''' the file stream id
     ''' </returns>
     <ExportAPI("open")>
-    Public Function open(file As String) As Integer
-        Dim intptr As Integer
-        NetCDF.nc_open(file, OpenMode.NC_NOWRITE, intptr)
-        Return intptr
-    End Function
-
-    <ExportAPI("dim_len")>
-    Public Function dimLen(nc As Integer, name As String) As Integer
-        Dim dimid As Integer = 0
-        Dim dimSize As Integer
-        NetCDF.nc_inq_dimid(nc, name, dimid)
-        NetCDF.nc_inq_dimlen(nc, dimid, dimSize)
-        Return dimSize
+    Public Function open(file As String) As DataReader
+        Return New DataReader(file)
     End Function
 
     ''' <summary>
@@ -40,15 +28,7 @@ Module CDF
     ''' <param name="name"></param>
     ''' <returns></returns>
     <ExportAPI("var_data")>
-    Public Function getData(nc As Integer, name As String) As Object
-        Dim var_id As Integer
-        Dim dimid As Integer
-        Dim attrs As Integer
-        Dim type As CDFDataTypes
-        NetCDF.nc_inq_varid(nc, name, var_id)
-        NetCDF.nc_inq_varndims(nc, var_id, dimid)
-        NetCDF.nc_inq_varnatts(nc, var_id, attrs)
-        NetCDF.nc_inq_vartype(nc, var_id, type)
-
+    Public Function getData(nc As DataReader, name As String) As Object
+        Return nc.GetData(name)
     End Function
 End Module
---
+++ src/data/CDF.PInvoke/DataReader.vb
@@ -0,0 +1,45 @@
+﻿
+Imports Microsoft.VisualBasic.DataStorage.netCDF.Components
+
+''' <summary>
+''' netcdf data reader model
+''' </summary>
+Public Class DataReader : Implements IDisposable
+
+    Dim disposedValue As Boolean
+    Dim handle As Integer
+
+    Sub New(file As String)
+        NetCDF.nc_open(file, OpenMode.NC_NOWRITE, ncidp:=handle)
+    End Sub
+
+    Public Function GetData(name As String) As ICDFDataVector
+
+    End Function
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                NetCDF.nc_close(handle)
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
