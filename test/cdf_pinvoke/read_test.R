9ad60892c0cb9645762a0f315c29ad18bfa80862
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-04 20:08:23 +0800

    try to make bugs fixed for the vector cast

--- test/cdf_pinvoke/read_test.R
+++ test/cdf_pinvoke/read_test.R
@@ -5,4 +5,5 @@ imports "CDF" from "Darwinism";
 const nc = CDF::open("E:\\GCModeller\\src\\runtime\\Darwinism\\src\\data\\CDF.PInvoke\\temperature.nc");
 const tmin = CDF::var_data(nc, "tmin");
 
-print(tmin);
+print(vars(nc));
+print(as.numeric(tmin));
