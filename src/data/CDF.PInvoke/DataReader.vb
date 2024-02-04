e1703b8481e366268a82140fb95879c880be7382
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-04 19:33:43 +0800

    set global error number

--- src/data/CDF.PInvoke/DataReader.vb
+++ src/data/CDF.PInvoke/DataReader.vb
@@ -13,18 +13,20 @@ Public Class DataReader : Implements IDisposable
     Dim handle As Integer
     Dim varnames As New Dictionary(Of String, Integer)
 
-    Sub New(file As String)
-        Dim flag = NetCDF.nc_open(file, OpenMode.NC_NOWRITE, ncidp:=handle)
+    Public ReadOnly Property error_no As Integer
 
-        If flag = 0 Then
+    Sub New(file As String)
+        _error_no = NetCDF.nc_open(file, OpenMode.NC_NOWRITE, ncidp:=handle)
+
+        If _error_no = 0 Then
             Dim varids As Integer() = New Integer(2048) {}
             Dim nvars As Integer
             Dim name As New StringBuilder
 
-            NetCDF.nc_inq_varids(handle, nvars, varids)
+            _error_no = NetCDF.nc_inq_varids(handle, nvars, varids)
 
             For i As Integer = 0 To nvars - 1
-                NetCDF.nc_inq_varname(handle, varids(i), name)
+                _error_no = NetCDF.nc_inq_varname(handle, varids(i), name)
                 varnames(name.ToString) = varids(i)
                 name.Clear()
             Next
@@ -42,17 +44,17 @@ Public Class DataReader : Implements IDisposable
         Dim ndims As Integer
         Dim dimSize As Integer
 
-        NetCDF.nc_inq_vartype(handle, id, type)
-        NetCDF.nc_inq_varndims(handle, id, ndims)
+        _error_no = NetCDF.nc_inq_vartype(handle, id, type)
+        _error_no = NetCDF.nc_inq_varndims(handle, id, ndims)
 
         Select Case type
             Case CDFDataTypes.NC_FLOAT
                 Dim v As Single() = New Single(1024) {}
-                NetCDF.nc_get_var_float(handle, id, v)
+                _error_no = NetCDF.nc_get_var_float(handle, id, v)
                 Return CType(v, floats)
             Case CDFDataTypes.NC_DOUBLE
                 Dim v As Double() = New Double(1024) {}
-                NetCDF.nc_get_var_double(handle, id, v)
+                _error_no = NetCDF.nc_get_var_double(handle, id, v)
                 Return CType(v, doubles)
             Case CDFDataTypes.NC_CHAR
             Case CDFDataTypes.NC_INT
