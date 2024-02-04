db65bf07a37a1c21034f342a51f1396a60b47759
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-04 17:47:15 +0800

    try to read a vector

--- src/data/CDF.PInvoke/DataReader.vb
+++ src/data/CDF.PInvoke/DataReader.vb
@@ -1,6 +1,8 @@
 ﻿
 Imports System.Text
 Imports Microsoft.VisualBasic.DataStorage.netCDF.Components
+Imports Microsoft.VisualBasic.DataStorage.netCDF.Data
+Imports Microsoft.VisualBasic.DataStorage.netCDF.DataVector
 
 ''' <summary>
 ''' netcdf data reader model
@@ -9,30 +11,55 @@ Public Class DataReader : Implements IDisposable
 
     Dim disposedValue As Boolean
     Dim handle As Integer
-    Dim varnames As String()
+    Dim varnames As New Dictionary(Of String, Integer)
 
     Sub New(file As String)
         Dim flag = NetCDF.nc_open(file, OpenMode.NC_NOWRITE, ncidp:=handle)
 
-        If flag <> 0 Then
-            Dim varids As Integer() = Nothing
+        If flag = 0 Then
+            Dim varids As Integer() = New Integer(2048) {}
             Dim nvars As Integer
             Dim name As New StringBuilder
 
             NetCDF.nc_inq_varids(handle, nvars, varids)
 
-            varnames = New String(nvars - 1) {}
-
             For i As Integer = 0 To nvars - 1
                 NetCDF.nc_inq_varname(handle, varids(i), name)
-                varnames(i) = name.ToString
+                varnames(name.ToString) = varids(i)
                 name.Clear()
             Next
         End If
     End Sub
 
     Public Function GetData(name As String) As ICDFDataVector
+        Dim id As Integer = varnames.TryGetValue(name, [default]:=-1)
 
+        If id < 0 Then
+            Return Nothing
+        End If
+
+        Dim type As CDFDataTypes = CDFDataTypes.undefined
+        Dim ndims As Integer
+        Dim dimSize As Integer
+
+        NetCDF.nc_inq_vartype(handle, id, type)
+        NetCDF.nc_inq_varndims(handle, id, ndims)
+
+        Select Case type
+            Case CDFDataTypes.NC_FLOAT
+                Dim v As Single() = New Single(1024) {}
+                NetCDF.nc_get_var_float(handle, id, v)
+                Return CType(v, floats)
+            Case CDFDataTypes.NC_DOUBLE
+                Dim v As Double() = New Double(1024) {}
+                NetCDF.nc_get_var_double(handle, id, v)
+                Return CType(v, doubles)
+            Case CDFDataTypes.NC_CHAR
+            Case CDFDataTypes.NC_INT
+            Case CDFDataTypes.NC_SHORT
+            Case Else
+                Throw New NotImplementedException(type.ToString)
+        End Select
     End Function
 
     Protected Overridable Sub Dispose(disposing As Boolean)
