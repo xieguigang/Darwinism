71ab9ba0ab6adb58d44c7a8e0c13261f1bc89588
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-15 14:36:00 +0800

    run msbuild

---
+++ @export/CDF.d.ts
@@ -0,0 +1,30 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "CDF" from "Darwinism";
+//
+// ref=Darwinism.CDF@Darwinism, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace CDF {
+   /**
+    * open a connection to the netcdf file
+    * 
+    * 
+     * @param file -
+     * @return the file stream id
+   */
+   function open(file: string): object;
+   /**
+    * get variable data from given file
+    * 
+    * 
+     * @param nc -
+     * @param name -
+   */
+   function var_data(nc: object, name: string): any;
+   /**
+   */
+   function vars(nc: object): string;
+}
---
+++ man/Darwinism/CDF/CDF.md
@@ -0,0 +1,7 @@
+# CDF
+
+
+
++ [open](CDF/open.1) open a connection to the netcdf file
++ [vars](CDF/vars.1) 
++ [var_data](CDF/var_data.1) get variable data from given file
