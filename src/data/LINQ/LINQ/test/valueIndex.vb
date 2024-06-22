d643548ca5780e9fc1758df0cf70e22080ebeea9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-22 21:20:34 +0800

    fix of the name reference conflicts

--- src/data/LINQ/LINQ/test/valueIndex.vb
+++ src/data/LINQ/LINQ/test/valueIndex.vb
@@ -1,64 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::979575c79062c4e6877a3dca6c34b04d, src\data\LINQ\LINQ\test\valueIndex.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 16
-    '    Code Lines: 12 (75.00%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 4 (25.00%)
-    '     File Size: 498 B
+' Summaries:
 
 
-    ' Module valueIndex
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 16
+'    Code Lines: 12 (75.00%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 4 (25.00%)
+'     File Size: 498 B
+
+
+' Module valueIndex
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports LINQ
 Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
 
-Public Module valueIndex
+Public Module valueIndex2
 
     Sub Main()
         Dim pool = Enumerable.Range(0, 10000000).Select(Function(a) randf.NextDouble(0, 2000)).ToArray
-        Dim index As RangeIndex(Of Double) = DoubleIndex().IndexData(pool)
+        Dim index As RangeIndex(Of Double) = ValueIndex.DoubleIndex().IndexData(pool)
 
         Dim search1 = index.Search(100).ToArray
         Dim search2 = index.Search(99, 103).ToArray
