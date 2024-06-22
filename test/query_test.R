5adc9f3e1dd1f2491e1cfe47b19897cb1373a887
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-22 22:47:30 +0800

    config debugh profile

---
+++ test/query_test.R
@@ -0,0 +1,18 @@
+require(Darwinism);
+
+imports "memory_query" from "Darwinism";
+
+let test_data = read.csv(file.path(@dir, "..", "proj_stats.csv"), row.names = NULL);
+
+test_data[, "proj_name"] = basename(test_data$proj);
+test_data[, "filename"] = basename(test_data$files);
+
+print("get input data table:");
+print(test_data, max.print = 13);
+
+let table = memory_query::load(test_data)
+|> fulltext(["proj","files"])
+|> hashindex(["proj_name", "filename"])
+;
+
+
