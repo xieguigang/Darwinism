cfd3dcd03373250da27445f8deefb2bbc9d7c371
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-22 19:39:20 +0800

    make imrpvoementsof the document index

---
+++ src/data/LINQ/LINQ/Runtime/FullTextSearch/InMemoryDocuments.vb
@@ -0,0 +1,24 @@
+﻿Public Class InMemoryDocuments : Inherits DocumentPool
+
+    ReadOnly documents As New List(Of String)
+
+    Public Overrides Sub Save(doc As String)
+        Call documents.Add(doc)
+    End Sub
+
+    Public Overrides Sub WriteIndex(index As InvertedIndex)
+        ' do nothing
+    End Sub
+
+    Public Overrides Sub Dispose()
+        Call documents.Clear()
+    End Sub
+
+    Public Overrides Function GetIndex() As InvertedIndex
+        Return New InvertedIndex
+    End Function
+
+    Public Overrides Function GetDocument(id As Integer) As String
+        Return documents(id)
+    End Function
+End Class
