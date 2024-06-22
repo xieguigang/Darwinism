ac1946db0d7735c4aa68c334888a09c9e090f1eb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-22 20:50:49 +0800

    test of the value range index

---
+++ src/data/LINQ/LINQ/Runtime/Indexing/IndexExtensions.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.ValueTypes
+
+Public Module IndexExtensions
+
+    Public Function IntegerIndex() As RangeIndex(Of Integer)
+        Return New RangeIndex(Of Integer)(Function(i) CDbl(i))
+    End Function
+
+    Public Function DoubleIndex() As RangeIndex(Of Double)
+        Return New RangeIndex(Of Double)(Function(x) x)
+    End Function
+
+    Public Function DateIndex() As RangeIndex(Of Date)
+        Return New RangeIndex(Of Date)(Function(x) x.UnixTimeStamp)
+    End Function
+
+End Module
---
+++ src/data/LINQ/LINQ/test/valueIndex.vb
@@ -0,0 +1,16 @@
+﻿Imports LINQ
+Imports randf = Microsoft.VisualBasic.Math.RandomExtensions
+
+Public Module valueIndex
+
+    Sub Main()
+        Dim pool = Enumerable.Range(0, 10000000).Select(Function(a) randf.NextDouble(-99, 10000)).ToArray
+        Dim index As RangeIndex(Of Double) = DoubleIndex().IndexData(pool)
+
+        Dim search1 = index.Search(100).ToArray
+        Dim search2 = index.Search(90, 300).ToArray
+        Dim search3 = index.Search(-50).ToArray
+
+        Pause()
+    End Sub
+End Module
