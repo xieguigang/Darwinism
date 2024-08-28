91789c4b1c23a194c6e59d130018255060e3a968
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-28 17:09:34 +0800

    a general model for GA method?

---
+++ src/DataScience/DataMining/GA.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF.Population
+Imports Microsoft.VisualBasic.MachineLearning.Darwinism.Models
+
+Public Class GAIPCParallelPopulationCompute(Of chr As {Class, Chromosome(Of chr)}) : Inherits ParallelComputeFitness(Of chr)
+
+    Public Overrides Function ComputeFitness(comparator As FitnessPool(Of chr), source As PopulationCollection(Of chr)) As IEnumerable(Of NamedValue(Of Double))
+        Throw New NotImplementedException()
+    End Function
+End Class
