dd3ae0520cfbcd63c957f3526eea104a9c2b7a57
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-14 10:38:40 +0800

    implements of the general clr vector indexer

--- src/data/LINQ/LINQ/MemoryQuery/MemoryPool.vb
+++ src/data/LINQ/LINQ/MemoryQuery/MemoryPool.vb
@@ -3,6 +3,52 @@ Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Scripting
 
 ''' <summary>
+''' index of the general clr object array.
+''' </summary>
+Public Class MemoryPool : Inherits MemoryIndex
+
+    ReadOnly vector As DataObjectVector
+
+    Sub New(data As Array)
+        vector = New DataObjectVector(data)
+    End Sub
+
+    Protected Overrides Function GetData(Of V)(field As String) As V()
+        Dim prop As PropertyInfo = vector.GetProperty(field)
+
+        If prop.PropertyType Is GetType(V) Then
+            Return vector(field)
+        Else
+            Dim pull As Array = vector(field)
+            Dim cast As V() = pull.CTypeDynamic(GetType(V))
+
+            Return cast
+        End If
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="filter"></param>
+    ''' <returns>
+    ''' this function will returns nothing if query filter has no result
+    ''' </returns>
+    Public Function Query(filter As IEnumerable(Of Query)) As Array()
+        Dim index As Integer() = GetIndex(filter)
+
+        If index.IsNullOrEmpty Then
+            Return Nothing
+        End If
+
+        Dim pool As Array = vector.RawArray
+
+        Return index _
+            .Select(Function(i) pool(i)) _
+            .ToArray
+    End Function
+End Class
+
+''' <summary>
 ''' Index of the generic clr object
 ''' </summary>
 Public Class MemoryPool(Of T) : Inherits MemoryIndex
