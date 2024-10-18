10b2794a0e4253fcf2acf08526972082563379dd
谢桂纲 <gg.xie@bionovogene.com>
2024-10-18 14:40:35 +0800

    removes duplicated class index

--- src/data/LINQ/LINQ/MemoryQuery/MemoryPool.vb
+++ src/data/LINQ/LINQ/MemoryQuery/MemoryPool.vb
@@ -68,11 +68,6 @@ Imports Microsoft.VisualBasic.Scripting
 Public Class MemoryPool : Inherits MemoryIndex
 
     ReadOnly vector As DataObjectVector
-    ''' <summary>
-    ''' the raw input pool data
-    ''' </summary>
-    ReadOnly pool As Array
-
     ReadOnly sub_vector As New Dictionary(Of String, DataObjectVector)
 
     Sub New(data As Array, Optional [property] As String = Nothing)
@@ -153,67 +148,13 @@ Public Class MemoryPool : Inherits MemoryIndex
     ''' <returns>
     ''' this function will returns nothing if query filter has no result
     ''' </returns>
-    Public Function Query(filter As IEnumerable(Of Query)) As Object()
+    Public Function Query(filter As IEnumerable(Of Query)) As Array
         Dim index As Integer() = GetIndex(filter)
 
         If index.IsNullOrEmpty Then
             Return Nothing
         End If
 
-        Return index _
-            .Select(Function(i) pool(i)) _
-            .ToArray
-    End Function
-End Class
-
-''' <summary>
-''' Index of the generic clr object
-''' </summary>
-Public Class MemoryPool(Of T) : Inherits MemoryIndex
-
-    ReadOnly pool As T()
-    ReadOnly vector As DataValue(Of T)
-
-    Sub New(data As T())
-        pool = data
-        vector = New DataValue(Of T)(data)
-    End Sub
-
-    Protected Overrides Function GetData(Of V)(field As String) As V()
-        Dim prop As PropertyInfo = vector.GetProperty(field)
-
-        If prop.PropertyType Is GetType(V) Then
-            Return vector(field)
-        Else
-            Dim pull As Array = vector(field)
-            Dim cast As V() = pull.CTypeDynamic(GetType(V))
-
-            Return cast
-        End If
-    End Function
-
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="filter"></param>
-    ''' <returns>
-    ''' this function will returns nothing if query filter has no result
-    ''' </returns>
-    Public Function Query(filter As IEnumerable(Of Query)) As T()
-        Dim index As Integer() = GetIndex(filter)
-
-        If index.IsNullOrEmpty Then
-            Return Nothing
-        End If
-
-        Return index _
-            .Select(Function(i) pool(i)) _
-            .ToArray
-    End Function
-
-    Protected Overrides Function CheckScalar(field As String) As Boolean
-        Dim prop As PropertyInfo = vector.GetProperty(field)
-        Dim is_scalar As Boolean = Not prop.PropertyType.IsArray
-        Return is_scalar
+        Return vector(index)
     End Function
 End Class
