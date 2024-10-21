201c22319eccb46829dc0da19039fc0bd36ca8c9
谢桂纲 <gg.xie@bionovogene.com>
2024-10-21 09:03:42 +0800

    create text similarity search index

---
+++ src/data/LINQ/LINQ/Runtime/Indexing/LevenshteinIndex.vb
@@ -0,0 +1,20 @@
+﻿Public Class LevenshteinIndex : Inherits SearchIndex
+
+
+
+    Public Sub New(documents As DocumentPool)
+        MyBase.New(documents)
+    End Sub
+
+    Public Overrides Sub Indexing(doc As String)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub Indexing(doc As String, id As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Protected Overrides Sub IndexingOneDocument(data() As String)
+        Throw New NotImplementedException()
+    End Sub
+End Class
