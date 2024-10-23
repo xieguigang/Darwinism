7471e86ca2792443cf486113e962c3657596f171
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-23 11:00:18 +0800

    make error fixed for write binary data

--- src/data/LINQ/RQL/SearchIndex/HashIndexFile.vb
+++ src/data/LINQ/RQL/SearchIndex/HashIndexFile.vb
@@ -1,4 +1,5 @@
-﻿Imports LINQ
+﻿Imports System.IO
+Imports LINQ
 Imports Microsoft.VisualBasic.DataStorage.HDSPack
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
 Imports Microsoft.VisualBasic.MIME.application.json
@@ -8,7 +9,14 @@ Public Class HashIndexFile
 
     Public Shared Sub WriteIndex(index As TermHashIndex, root As String, s As StreamPack)
         Call s.WriteText(index.GetDocumentMaps.GetJson, $"{root}/documentMaps.json")
-        Call s.WriteText(BSONFormat.SafeGetBuffer(index.GetHashIndex.CreateJSONElement), $"{root}/hashMaps.bson")
+
+        Using buf As Stream = s.OpenFile($"{root}/hashMaps.bson", FileMode.Create, FileAccess.Write)
+            Dim bytes As MemoryStream = BSONFormat.SafeGetBuffer(index.GetHashIndex.CreateJSONElement)
+
+            Call bytes.Seek(Scan0, SeekOrigin.Begin)
+            Call bytes.Flush()
+            Call bytes.CopyTo(buf)
+        End Using
     End Sub
 
 End Class
