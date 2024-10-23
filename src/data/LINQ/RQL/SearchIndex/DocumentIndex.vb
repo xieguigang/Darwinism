ad97bb5133a89ae0815414f5773d8c1e930c5ffd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-23 09:16:09 +0800

    load json object

---
+++ src/data/LINQ/RQL/SearchIndex/DocumentIndex.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.MIME.application.json.BSON
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+
+Public Class DocumentIndex
+
+    ReadOnly offsets As Dictionary(Of UInteger, Long)
+    ReadOnly document As Stream
+    ReadOnly text As StreamReader
+
+    Sub New(s As Stream)
+
+    End Sub
+
+    Public Function ReadLine(id As UInteger) As String
+        Dim offset As Long = offsets(id)
+        text.DiscardBufferedData()
+        document.Seek(offset, SeekOrigin.Begin)
+        Return text.ReadLine
+    End Function
+
+    Public Function GetSubBuffer(id As UInteger) As Stream
+        Dim offset As Long = offsets(id)
+        Dim s As New SubStream(document, offset, document.Length - offset)
+        Return s
+    End Function
+
+    Public Function GetObject(id As UInteger) As JsonObject
+        Dim s As Stream = GetSubBuffer(id)
+        Dim obj As JsonObject = BSONFormat.Load(s, leaveOpen:=True)
+        Return obj
+    End Function
+
+End Class
---
+++ src/data/LINQ/RQL/SearchIndex/HashIndexFile.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+Imports LINQ
+
+Public Class HashIndexFile
+
+    Public Shared Sub WriteIndex(index As TermHashIndex, s As Stream)
+
+    End Sub
+
+End Class
