0e04903f22f86081e360506c1107c034c011e5e3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-07 09:26:30 +0800

    implements a internal query interpreter

--- src/data/LINQ/LINQ/Interpreter/QueryInterpreter.vb
+++ src/data/LINQ/LINQ/Interpreter/QueryInterpreter.vb
@@ -1,60 +1,64 @@
 ﻿#Region "Microsoft.VisualBasic::8254e340e7781de9c93e502de427f388, src\data\LINQ\LINQ\Interpreter\QueryInterpreter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 17
-    '    Code Lines: 8 (47.06%)
-    ' Comment Lines: 3 (17.65%)
-    '    - Xml Docs: 100.00%
-    ' 
-    '   Blank Lines: 6 (35.29%)
-    '     File Size: 332 B
+' Summaries:
 
 
-    '     Class QueryInterpreter
-    ' 
-    '         Properties: env
-    ' 
-    '         Function: Exec
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 17
+'    Code Lines: 8 (47.06%)
+' Comment Lines: 3 (17.65%)
+'    - Xml Docs: 100.00%
+' 
+'   Blank Lines: 6 (35.29%)
+'     File Size: 332 B
+
+
+'     Class QueryInterpreter
+' 
+'         Properties: env
+' 
+'         Function: Exec
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports LINQ.Interpreter.Query
+Imports LINQ.Language
 Imports LINQ.Runtime
+Imports LINQ.Script
+Imports Microsoft.VisualBasic.My.JavaScript
 
 Namespace Interpreter
 
@@ -65,8 +69,14 @@ Namespace Interpreter
 
         Public Property env As Environment
 
-        Public Function Exec(query As String) As Object
+        Public Function Exec(query_str As String) As Object
+            Dim tokens As Token() = LINQ.Language.GetTokens(query_str).ToArray
+            Dim query As ProjectionExpression = tokens.PopulateQueryExpression
+            Dim env As New GlobalEnvironment(New Registry)
+            Dim context As New ExecutableContext With {.env = env, .throwError = True}
+            Dim result As JavaScriptObject() = query.Exec(context)
 
+            Return result
         End Function
 
     End Class
