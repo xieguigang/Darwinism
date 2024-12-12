cddd0cb5c703555c13bfccc7607c46e3b30126fc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-12 10:27:51 +0800

    Update HDF.vb

--- src/Darwinism/Data/HDF.vb
+++ src/Darwinism/Data/HDF.vb
@@ -1,8 +1,16 @@
 ﻿
+Imports HDF.PInvoke
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("HDF")>
 Module HDF
+
+    <ExportAPI("open")>
+    Public Function open(file As String) As Long
+        Dim fileId As Long = H5F.open(file, H5F.ACC_RDONLY)
+        Return fileId
+    End Function
 
 
 
