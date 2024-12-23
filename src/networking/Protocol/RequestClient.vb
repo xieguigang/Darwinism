1a3ddcbf6d77a7f643c53493675fd16904a7d4c7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-23 14:41:35 +0800

    make improvements of the services module debug

---
+++ src/networking/Protocol/RequestClient.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Darwinism.IPC.Networking.Tcp
+Imports Microsoft.VisualBasic.Net
+Imports Microsoft.VisualBasic.Parallel
+
+Namespace Protocols
+
+    Public Class RequestClient : Implements IRequestClient
+
+        ReadOnly host As IPEndPoint
+
+        Dim timeout_sec As Double = 6
+
+        Sub New(host As IPEndPoint)
+            Me.host = host
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function HandleRequest(req As RequestStream) As RequestStream Implements IRequestClient.SendMessage
+            Return New TcpRequest(host).SetTimeOut(TimeSpan.FromSeconds(timeout_sec)).SendMessage(req)
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return host.ToString
+        End Function
+    End Class
+End Namespace
