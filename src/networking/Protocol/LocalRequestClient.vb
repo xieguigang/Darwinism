b97d1922fd3865661ee08b67978983fef6a4da20
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-23 10:56:36 +0800

    create services local debug helper

---
+++ src/networking/Protocol/LocalRequestClient.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net
+Imports Darwinism.IPC.Networking.Protocols.Reflection
+Imports Microsoft.VisualBasic.Parallel
+
+''' <summary>
+''' helper for run services module debug
+''' </summary>
+Public Class LocalRequestClient
+
+    ReadOnly fakeLocal As IPEndPoint
+    ReadOnly host As ProtocolHandler
+
+    Sub New(host As ProtocolHandler)
+        Me.host = host
+        Me.fakeLocal = New IPEndPoint(0, 1)
+    End Sub
+
+    Public Function SendMessage(message As RequestStream) As RequestStream
+        Dim pull As New List(Of Byte)
+        Dim responseData = host.HandleRequest(message, fakeLocal)
+
+        For Each buf As Byte() In responseData.GetBlocks
+            Call pull.AddRange(buf)
+        Next
+
+        Return New RequestStream(0, 200, pull.ToArray)
+    End Function
+End Class
