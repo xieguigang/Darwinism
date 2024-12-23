2f40f44554ead0acc89c7edad2d2a5138353b8bf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-23 10:44:47 +0800

    split source file into multiple parts

---
+++ src/networking/Protocol/Abstractdelegates.vb
@@ -0,0 +1,24 @@
+﻿Imports Darwinism.IPC.Networking.Protocols.Reflection
+Imports Darwinism.IPC.Networking.Tcp
+Imports Microsoft.VisualBasic.Parallel
+
+''' <summary>
+''' Object for handles the request <see cref="ProtocolAttribute"/>.
+''' </summary>
+Public MustInherit Class IProtocolHandler
+
+    MustOverride ReadOnly Property ProtocolEntry As Long
+    MustOverride Function HandleRequest(request As RequestStream, remoteDevcie As System.Net.IPEndPoint) As BufferPipe
+
+End Class
+
+#Region "Delegate Abstract Interface"
+
+Public Delegate Function SendMessageInvoke(Message As String) As String
+
+Public Delegate Sub ForceCloseHandle(socket As StateObject)
+
+
+#End Region
+
+Public Delegate Sub ProcessMessagePush(message As RequestStream)
--- src/networking/Protocol/IProtocolHandler.vb
+++ src/networking/Protocol/IProtocolHandler.vb
@@ -73,32 +73,10 @@
 #End Region
 
 Imports System.Runtime.CompilerServices
-Imports Darwinism.IPC.Networking.Protocols.Reflection
 Imports Darwinism.IPC.Networking.Tcp
 Imports Microsoft.VisualBasic.Net
 Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Parallel
-
-''' <summary>
-''' Object for handles the request <see cref="ProtocolAttribute"/>.
-''' </summary>
-Public MustInherit Class IProtocolHandler
-
-    MustOverride ReadOnly Property ProtocolEntry As Long
-    MustOverride Function HandleRequest(request As RequestStream, remoteDevcie As System.Net.IPEndPoint) As BufferPipe
-
-End Class
-
-#Region "Delegate Abstract Interface"
-
-Public Delegate Function SendMessageInvoke(Message As String) As String
-
-Public Delegate Sub ForceCloseHandle(socket As StateObject)
-
-
-#End Region
-
-Public Delegate Sub ProcessMessagePush(message As RequestStream)
 
 <HideModuleName>
 Public Module Extensions
