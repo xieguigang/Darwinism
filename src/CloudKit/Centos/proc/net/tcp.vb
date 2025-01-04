9778351529646aadeee28d26d80a084496577e00
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-04 23:14:42 +0800

    add file model

---
+++ src/CloudKit/Centos/proc/net/tcp.vb
@@ -0,0 +1,90 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+
+Namespace proc.net
+
+    ' In Linux, the /proc filesystem is a virtual filesystem that provides an interface to
+    ' kernel data structures. It is not stored on disk, but is created dynamically when the
+    ' system boots up and is removed when the system shuts down. The /proc filesystem
+    ' contains a wealth of information about the system, including process information,
+    ' memory usage, hardware configuration, and network status.
+
+    ' The /proc/net/tcp and /proc/net/udp files are part of the /proc filesystem and provide
+    ' detailed information about the TCP and UDP sockets that are currently in use by the
+    ' Linux kernel.
+
+    ''' <summary>
+    ''' /proc/net/tcp
+    ''' </summary>
+    ''' <remarks>
+    ''' This file contains information about all TCP sockets that are currently open on 
+    ''' the system. Each line in the file represents a TCP socket, and the fields in 
+    ''' each line provide details about the socket’s state, the local and remote addresses 
+    ''' and ports, and other related information.
+    ''' </remarks>
+    Public Class tcp
+
+        ''' <summary>
+        ''' The socket’s slot (index) in the kernel’s socket table.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property sl As Integer
+        ''' <summary>
+        ''' The local IP address and port number (in hexadecimal).
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property local_address As String
+        ''' <summary>
+        ''' The remote IP address and port number (in hexadecimal), or 00000000:0000 if not connected.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property rem_address As String
+        ''' <summary>
+        ''' The socket’s state (e.g., 0A for listening, 01 for established).
+        ''' </summary>
+        ''' <returns></returns>
+        Public Overridable Property st As String
+        ''' <summary>
+        ''' The amount of data in the transmit queue.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property tx_queue As String
+        ''' <summary>
+        ''' The amount of data in the receive queue.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property rx_queue As String
+        ''' <summary>
+        ''' Timing and retransmission information.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property tr As String
+        ''' <summary>
+        ''' Timing and retransmission information.
+        ''' </summary>
+        ''' <returns></returns>
+        <Column("tm->when")>
+        Public Property tm_when As String
+        ''' <summary>
+        ''' Timing and retransmission information.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property retrnsmt As String
+        ''' <summary>
+        ''' The user ID of the process that owns the socket.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property uid As Integer
+        ''' <summary>
+        ''' The socket’s timeout value.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property timeout As Integer
+        ''' <summary>
+        ''' The inode number of the socket. This can be used to find the process using 
+        ''' the socket by looking at ``/proc/[pid]/fdinfo/`` for the inode.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property inode As String
+
+    End Class
+End Namespace
---
+++ src/CloudKit/Centos/proc/net/udp.vb
@@ -0,0 +1,32 @@
+﻿Namespace proc.net
+
+    ''' <summary>
+    ''' /proc/net/udp
+    ''' 
+    ''' This file is similar to /proc/net/tcp, but it provides information about UDP sockets. 
+    ''' Since UDP is connectionless, some of the fields that are relevant to TCP connections
+    ''' (like st for socket state) are not applicable or are set to default values.
+    ''' </summary>
+    Public Class udp : Inherits tcp
+
+        ''' <summary>
+        ''' The socket’s state. For UDP, this is typically 07, which represents a socket 
+        ''' that is waiting to receive packets.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Overrides Property st As String
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property ref As Integer
+        Public Property pointer As String
+        ''' <summary>
+        ''' The number of packets dropped by the socket.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property drops As String
+
+    End Class
+End Namespace
