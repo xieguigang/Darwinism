bd09b0b767bb3518818b19b9e840f69de09eb6dc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-04 22:43:03 +0800

    fix of the invalid component reference

--- src/computing/Parallel/test/clr_parallelTask_demo.vb
+++ src/computing/Parallel/test/clr_parallelTask_demo.vb
@@ -1,68 +1,67 @@
 ﻿#Region "Microsoft.VisualBasic::fd7199a6e90b5733e27b3b03bcedb7b2, src\computing\Parallel\test\clr_parallelTask_demo.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 59
-    '    Code Lines: 43 (72.88%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 16 (27.12%)
-    '     File Size: 1.88 KB
+' Summaries:
 
 
-    ' Module clr_parallelTask_demo
-    ' 
-    '     Function: compute_function, generateDemoData
-    ' 
-    '     Sub: Main2, runParallel
-    ' 
-    ' Class vectorData
-    ' 
-    '     Properties: Data
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 59
+'    Code Lines: 43 (72.88%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 16 (27.12%)
+'     File Size: 1.88 KB
+
+
+' Module clr_parallelTask_demo
+' 
+'     Function: compute_function, generateDemoData
+' 
+'     Sub: Main2, runParallel
+' 
+' Class vectorData
+' 
+'     Properties: Data
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports batch
+Imports Darwinism.HPC.Parallel.IpcStream
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Correlations
 Imports Microsoft.VisualBasic.MIME.application.json
-Imports Parallel
-Imports Parallel.IpcStream
 Imports rnd = Microsoft.VisualBasic.Math.RandomExtensions
 
 Module clr_parallelTask_demo
--- src/computing/Parallel/test/memoryTest.vb
+++ src/computing/Parallel/test/memoryTest.vb
@@ -1,62 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::d6174f18955f94afc64848699e9ca436, src\computing\Parallel\test\memoryTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 39
-    '    Code Lines: 25 (64.10%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 14 (35.90%)
-    '     File Size: 1000 B
+' Summaries:
 
 
-    ' Module memoryTest
-    ' 
-    '     Sub: Main1
-    ' 
-    ' Class vec
-    ' 
-    '     Properties: v
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 39
+'    Code Lines: 25 (64.10%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 14 (35.90%)
+'     File Size: 1000 B
+
+
+' Module memoryTest
+' 
+'     Sub: Main1
+' 
+' Class vec
+' 
+'     Properties: v
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Darwinism.HPC.Parallel
 Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Parallel
 
 Module memoryTest
 
