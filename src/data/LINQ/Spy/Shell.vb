9fa74a3f8f8c2f48115c46d61dd269cc2d36c01a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-26 20:00:10 +0800

    fuix of the symbol reference error

--- src/data/LINQ/Spy/Shell.vb
+++ src/data/LINQ/Spy/Shell.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::968c358571379422469e97858060b42c, src\data\LINQ\Spy\Shell.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 67
-    '    Code Lines: 47 (70.15%)
-    ' Comment Lines: 9 (13.43%)
-    '    - Xml Docs: 33.33%
-    ' 
-    '   Blank Lines: 11 (16.42%)
-    '     File Size: 2.12 KB
+' Summaries:
 
 
-    ' Module LinqShell
-    ' 
-    '     Function: RunTerminal
-    ' 
-    '     Sub: ExecLinq
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 67
+'    Code Lines: 47 (70.15%)
+' Comment Lines: 9 (13.43%)
+'    - Xml Docs: 33.33%
+' 
+'   Blank Lines: 11 (16.42%)
+'     File Size: 2.12 KB
+
+
+' Module LinqShell
+' 
+'     Function: RunTerminal
+' 
+'     Sub: ExecLinq
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -60,7 +60,7 @@ Imports LINQ.Runtime
 Imports LINQ.Script
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.LineEdit
-Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Data.Framework.IO
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.My.JavaScript
 
@@ -105,7 +105,7 @@ Module LinqShell
 
         If result.CheckTabular Then
             ' result as table
-            Dim table As DataFrame = result.CreateTableDataSet
+            Dim table As DataFrameResolver = result.CreateTableDataSet
             ' print on the console
             Dim text As String()() = table _
                 .csv _
