a19dd4aafa2ee976fa05c046846c29d76a574d80
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-01 22:54:14 +0800

    fi of the module reference error

--- src/Darwinism/Math.vb
+++ src/Darwinism/Math.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::b923c74657f122907358850a71244a0d, src\Darwinism\Math.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 87
-    '    Code Lines: 57 (65.52%)
-    ' Comment Lines: 20 (22.99%)
-    '    - Xml Docs: 95.00%
-    ' 
-    '   Blank Lines: 10 (11.49%)
-    '     File Size: 3.36 KB
+' Summaries:
 
 
-    ' Module Math
-    ' 
-    '     Function: averageDistance, FindNeighbors
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 87
+'    Code Lines: 57 (65.52%)
+' Comment Lines: 20 (22.99%)
+'    - Xml Docs: 95.00%
+' 
+'   Blank Lines: 10 (11.49%)
+'     File Size: 3.36 KB
+
+
+' Module Math
+' 
+'     Function: averageDistance, FindNeighbors
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -56,6 +56,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.GraphTheory.KdTree.ApproximateNearNeighbor
 Imports Microsoft.VisualBasic.DataMining.KMeans
 Imports Microsoft.VisualBasic.Emit.Delegates
+Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Matrix
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
