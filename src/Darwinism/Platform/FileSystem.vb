bf3d043fb1787f86e517988bba776c2e86f5ec09
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-22 15:30:18 +0800

    export helper object

---
+++ src/Darwinism/Platform/FileSystem.vb
@@ -0,0 +1,14 @@
+﻿
+Imports Darwinism.OSSUtil
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("ossutil")>
+Module FileSystemTool
+
+    <ExportAPI("aspera")>
+    Public Function Aspera(server As String, Optional bin As String = "ascp") As Aspera
+        Return New Aspera(server, bin)
+    End Function
+
+End Module
