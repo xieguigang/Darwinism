ef616502343c939717b883a7071795b1577f5a3f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-11 16:18:51 +0800

    symbol renames

--- src/data/LINQ/LINQ/test/valueIndex.vb
+++ src/data/LINQ/LINQ/test/valueIndex.vb
@@ -95,7 +95,7 @@ Public Module valueIndex2
             Call index.AddString(name)
         Next
 
-        Dim test = index.FindSimilar("chococcus", 0.1)
+        Dim test = index.FindSimilar("chococcus", 0.1).ToArray
 
         Pause()
     End Sub
