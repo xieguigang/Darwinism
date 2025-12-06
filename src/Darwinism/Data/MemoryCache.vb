ffd7144c795a710719d140523af09be4a85fe113
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-07 04:11:11 +0800

    export the memory db type

---
+++ src/Darwinism/Data/MemoryCache.vb
@@ -0,0 +1,15 @@
+﻿Imports BucketDb
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("memory_cache")>
+<RTypeExport("memory_db", GetType(Buckets))>
+Public Module MemoryCache
+
+    <ExportAPI("new")>
+    Public Function newDb(dir As String) As Buckets
+        Return New Buckets(dir)
+    End Function
+
+End Module
