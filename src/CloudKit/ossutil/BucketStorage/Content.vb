0a81f9ebd4af1476b96436820e2747aedc424fd3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-10 17:32:44 +0800

    add oss data list

---
+++ src/CloudKit/ossutil/BucketStorage/Content.vb
@@ -0,0 +1,20 @@
+﻿Public Class Content
+
+    ''' <summary>
+    ''' file path
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Key As String
+    Public Property LastModified As String
+    Public Property ETag As String
+    Public Property Size As Long
+    Public Property StorageClass As String
+    Public Property Owner As Owner
+
+End Class
+
+Public Class Owner
+
+    Public Property ID As String
+
+End Class
---
+++ src/CloudKit/ossutil/BucketStorage/ListBucketResult.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Xml.Serialization
+
+<XmlRoot("ListBucketResult", [Namespace]:="http://s3.amazonaws.com/doc/2006-03-01/")>
+Public Class ListBucketResult
+
+    Public Property Name As String
+    Public Property Prefix As String
+    Public Property Marker As String
+    Public Property MaxKeys As Integer
+    Public Property IsTruncated As Boolean
+    <XmlElement("Contents")> Public Property Contents As Content()
+
+End Class
