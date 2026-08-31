928739514c7cfe10efb6d4c7c67032c722c9a55f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-31 16:58:37 +0800

    which find command path

---
+++ src/CloudKit/Centos/Bash.vb
@@ -0,0 +1,6 @@
+﻿Public Module Bash
+
+    Public Function which(command As String) As String
+        Return Strings.Trim(Interaction.Shell("which", command, verbose:=False))
+    End Function
+End Module
